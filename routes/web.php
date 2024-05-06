ccd66d9bb7190fa1a0a60a13290734e6e31646ee
Neil Anderson <njp.anderson@icloud.com>
2024-05-06 17:52:03 +0100

    Add livewire as dev requirement; ensure CSRF/cookie middleware is present in braid

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,40 @@
 <?php
 
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
 use Illuminate\Support\Facades\Route;
 
 use njpanderson\Braid\Http\Middleware\Authorize;
 use njpanderson\Braid\Http\Controllers\BraidController;
 use njpanderson\Braid\Http\Controllers\PatternToolsController;
 
-Route::middleware([
-    Authorize::class,
-    SubstituteBindings::class
-])->group(function() {
-    Route::get('/braid', [BraidController::class, 'index'])->name('braid.index');
-    Route::get('/braid/welcome', [BraidController::class, 'welcome'])->name('braid.welcome');
-    Route::get('/braid/menu', [BraidController::class, 'menu'])->name('braid.menu');
+use njpanderson\Braid\Livewire\Pattern as LivewirePattern;
 
-    Route::get('/braid/patterntools', [PatternToolsController::class, 'list'])
+Route::prefix('braid')->middleware([
+    EncryptCookies::class,
+    AddQueuedCookiesToResponse::class,
+    StartSession::class,
+    ShareErrorsFromSession::class,
+    SubstituteBindings::class,
+    ValidateCsrfToken::class,
+    Authorize::class
+])->group(function() {
+    Route::get('/', [BraidController::class, 'index'])->name('braid.index');
+    Route::get('/welcome', [BraidController::class, 'welcome'])->name('braid.welcome');
+    Route::get('/menu', [BraidController::class, 'menu'])->name('braid.menu');
+
+    Route::get('/patterntools', [PatternToolsController::class, 'list'])
         ->name('braid.patterntools');
 
-    Route::get('/braid/patterntools/tool/{braidTool}/{braidPattern}/{contextId?}', [
+    Route::get('/patterntools/tool/{braidTool}/{braidPattern}/{contextId?}', [
         PatternToolsController::class, 'tool'
     ])
         ->name('braid.patterntools');
 
-    Route::get('/braid/pattern/{patternId}/{contextId?}', [BraidController::class, 'pattern'])
+    Route::get('/pattern/{patternId}/{contextId?}', [BraidController::class, 'pattern'])
         ->name('braid.pattern');
 });
