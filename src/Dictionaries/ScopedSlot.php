83d9116c6aae5f7e5ab58aaca8628b52fa4052e7
Neil Anderson <njp.anderson@icloud.com>
2024-08-31 14:31:18 +0100

    Allow views here as well

--- src/Dictionaries/ScopedSlot.php
+++ src/Dictionaries/ScopedSlot.php
@@ -2,6 +2,7 @@
 
 namespace njpanderson\Braid\Dictionaries;
 
+use Illuminate\Contracts\View\View;
 use njpanderson\Braid\Contracts\ScopedSlot as Contract;
 use njpanderson\Braid\Traits\HasAttributes;
 use njpanderson\Braid\Traits\HasSlot;
@@ -12,7 +13,7 @@ class ScopedSlot implements Contract
         HasSlot;
 
     public function __construct(
-        string|null $slot = null,
+        string|View|null $slot = null,
         array $attributes = []
     ) {
         $this->setAttributes($attributes, true);
